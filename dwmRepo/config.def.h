e552708a7113df2f8796003f5b0034e831de6db5
dbochoa77 <dbochoa77@proton.me>
2025-10-04 16:33:36 -0700

    working on fixing it

--- dwmRepo/config.def.h
+++ dwmRepo/config.def.h
@@ -1,27 +1,21 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <X11/XF86keysym.h>
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 3;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar && 1 means top bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=12" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=12";
-
-/* Workplaces Color*/
-static const char col_gray1[]       = "#222222"; /*222222*/ /* XP Green: 81C046*/
-
-/*No idea*/
-static const char col_gray2[]       = "#444444"; /*444444*/
-
-/* Text Color */
-static const char col_gray3[]       = "#ffffff";   /* bbbbbb */
-
-static const char col_gray4[]       = "#eeeeee";
-
-/* Make block color */
-static const char col_cyan[]        = "#3B77BC"; /*XP Blue: #3B77BC*/
+static const int user_bh            = 35;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=14" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=14";
+static const char col_gray1[]       = "#000000"; /*222222*/ /* XP Green: 81C046*/
+static const char col_gray2[]       = "#000000"; /*444444*/ /*words*/
+static const char col_gray3[]       = "#000000";   /* bbbbbb */
+static const char col_gray4[]       = "#000000";
+static const char col_cyan[]        = "#00004e"; /*XP Blue: #3B77BC*/
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
@@ -72,11 +66,32 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *volumeMute[] = { "wpctl", "set-mute", "@DEFAULT_AUDIO_SINK@", "toggle", NULL };
+static const char *micmute[] = { "wpctl", "set-mute", "@DEFAULT_AUDIO_SOURCE@", "toggle", NULL };
+static const char *volumeUp[] = { "wpctl", "set-volume", "@DEFAULT_AUDIO_SINK@", "5%+", NULL };
+static const char *volumeDown[] = { "wpctl", "set-volume", "@DEFAULT_AUDIO_SINK@", "5%-", NULL };
+static const char *brightness_up[] = { "brightnessctl", "set", "10%+", NULL };
+static const char *brightness_down[] = { "brightnessctl", "set", "10%-", NULL };
+static const char *wifitoggle[] = { "nmcli", "radio", "wifi" , "toggle", NULL };
+static const char *settings[] = { "alacritty", "cd ~/blacknix", NULL };
+static const char *bluetoothtoggle[] = { "rfkill", "toggle" , "bluetooth" , "toggle", NULL }; 
+static const char *emacscmd[] = { "emacsclient", "-c", "-a", "emacs", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
+        { 0,				XF86XK_AudioMute,   spawn, {.v = volumeMute} },
+	{ 0,				XF86XK_AudioRaiseVolume,   spawn, {.v = volumeUp } }, 			  { 0,				  XF86XK_AudioLowerVolume,   spawn, {.v = volumeDown } },
+        { 0,				XF86XK_AudioRaiseVolume,   spawn, {.v = volumeUp } }, 			  { 0,				  XF86XK_AudioMicMute,   spawn, {.v = micmute } },
+
+	{ 0,  				XF86XK_MonBrightnessUp,    spawn, {.v = brightness_up} },
+        { 0,  				XF86XK_MonBrightnessDown,  spawn, {.v = brightness_down} },
+        { 0,  				XF86XK_WLAN,  spawn, {.v = wifitoggle } },
+	{ 0,  				XK_F9,  spawn, {.v = settings } },
+	{ 0,  				XK_F10,  spawn, {.v = bluetoothtoggle } }, 
+
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,	                XK_q,      spawn,          {.v = termcmd } },
+	{ MODKEY,			XK_e,	   spawn,          {.v = emacscmd}},
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
